f29d1331679287b5880ef60351e38b4fc9a27bc0
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-05-21 22:11:14 +0500

    Adds eslint

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hasan app
+          hasans app
         </p>
       </header>
     </div>
   );
 }
-
-export default App;
