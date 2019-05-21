8e232bec7db223f4de4f941036d5c84ac7e19642
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-05-21 22:46:30 +0500

    Adds react-jss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import withStyles from 'react-jss';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hasans app
-        </p>
-      </header>
-    </div>
-  );
-}
+const styles = {
+  mainPage: {
+    height: '100%',
+    background: 'blue',
+    paddingTop: '10px',
+  },
+};
+
+const App = ({ classes }) => (
+  <div className={classes.mainPage}>
+    <h1>hasan app</h1>
+  </div>
+);
+
+App.propTypes = {
+  classes: PropTypes.shape({
+    mainPage: PropTypes.object,
+  }).isRequired,
+};
+
+export default withStyles(styles)(App);
