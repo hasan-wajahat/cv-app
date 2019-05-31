0b447a97c0cf5b3a81ba867dc25621f5f04ec63f
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-05-31 18:27:53 +0500

    Updates

---
+++ precache-manifest.e35edd13d0cd24489124f5a34e885f1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da69d96a7de152ee8bb472aec5c6a9b",
+    "url": "/cv-app/index.html"
+  },
+  {
+    "revision": "d600b9d61ec8fab5f227",
+    "url": "/cv-app/static/css/main.ef0456cd.chunk.css"
+  },
+  {
+    "revision": "e82b162781203b3b58a7",
+    "url": "/cv-app/static/js/2.51c15be8.chunk.js"
+  },
+  {
+    "revision": "d600b9d61ec8fab5f227",
+    "url": "/cv-app/static/js/main.0b55d878.chunk.js"
+  },
+  {
+    "revision": "f8e0dec55cc7f0903dc7",
+    "url": "/cv-app/static/js/runtime~main.1f72d373.js"
+  },
+  {
+    "revision": "67637761d1293f816ab48c9ac1c1da67",
+    "url": "/cv-app/static/media/docker-logo.67637761.png"
+  },
+  {
+    "revision": "6075a4e0d9db2f1c8c7658f54c17b13e",
+    "url": "/cv-app/static/media/girl.6075a4e0.png"
+  },
+  {
+    "revision": "0bcb32c6d86cf5aa6cd6bc1435f8665f",
+    "url": "/cv-app/static/media/git-logo.0bcb32c6.png"
+  },
+  {
+    "revision": "5817d7eeea556e5e29616da16df7bcc6",
+    "url": "/cv-app/static/media/laravel-logo.5817d7ee.png"
+  },
+  {
+    "revision": "524a08c5679118e88cde86516e026df3",
+    "url": "/cv-app/static/media/node-logo.524a08c5.png"
+  },
+  {
+    "revision": "1ebfa17db577b2a8f5aaa4d5f1245eae",
+    "url": "/cv-app/static/media/react-logo.1ebfa17d.png"
+  },
+  {
+    "revision": "e8869d928dcc06894b1881f9cce3624a",
+    "url": "/cv-app/static/media/redux-logo.e8869d92.png"
+  }
+]);
